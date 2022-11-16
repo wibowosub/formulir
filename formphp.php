8e9c49f205e27e0a6cce903cbffc9082eb8108b0
hari wibowo <wibowosub@gmail.com>
2022-11-16 09:29:26 +0700

    first commit

---
+++ formphp.php
@@ -0,0 +1,20 @@
+<?php
+// ini komentar
+$teks="bahasa php";
+$angka=10;
+
+
+
+
+echo "hallo dari: $teks";
+echo "<br>";
+echo $angka;
+echo "<br>";
+
+
+
+?>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// ini komentar
+$teks="bahasa php";
+$angka=10;
+$pecahan=12.5454;
+$absen=true;
+
+
+
+echo "hallo dari: $teks";
+echo "<br>";
+echo $angka;
+echo "<br>";
+
+
+
+?>
+
+
+
+
